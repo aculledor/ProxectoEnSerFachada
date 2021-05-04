b1709b4dd241ceca46e0185242ea015db4b92a45
Abraham Trashorras <abraham.cocodin@gmail.com>
2021-05-04 06:59:03 +0200

    Implemented login, getUser, getMovie and half of getMovies

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,9 @@ import DATA from './data'
 let __instance = null
 
 export default class API {
-    #token = sessionStorage.getItem('token') || null
+    #token = localStorage.getItem('token') || null
 
+    //Singleton constructor
     static instance() {
         if(__instance == null)
             __instance = new API()
@@ -12,36 +13,69 @@ export default class API {
         return __instance
     }
 
-    async login(email, pass) {
-        // TODO fetch from API and if successful, store token from response headers
-        const user = DATA.users.find(u => u.email === email)
+    //Login fetch
+    async login(email = '', pass = '') {
+        const url = "/api/login";
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accepts': 'application/json'
+            },
+            body: JSON.stringify({
+                "email": email,
+                "password": pass
+            })
+        })
 
-        if(user.password === pass) {
+        if(response.ok) {
+            const body = await response.headers.get('authentication')
+            this.#token = body
             localStorage.setItem('user', email)
-            localStorage.setItem('token', 'TEST TOKEN')
-            this.#token = 'TEST TOKEN'
+            localStorage.setItem('token', this.#token)
             return true
-        } else {
-            return false
+
+
+            //console.log(body)
+            //const promise = await this.findUser('pruba@uno.com')
+            //const data = await promise.json()
+            //console.log(data)
         }
+        return false
+
     }
+
+    //Logout from client side
     async logout() {
         this.#token = null
         localStorage.clear()
 
         return true
     }
+
+    //Get all movies
     async findMovies(
         {
-            filter: { genre = '', title = '', status = '' } = { genre : '', title : '', status : '' },
-            sort,
-            pagination: {page = 0, size = 7} = { page: 0, size: 7 }
+            filter: { title= '', keywords = [''], genre= '', genres = [''], crew = [''], cast = [''], producers = [''], releaseDate = '', status= ''} =
+                {  title: '', keywords: [], genre: '', genres: [], crew: [], cast: [], producers: [], releaseDate: '', status: '' },
+            sort = [''],
+            pagination: {page = 0, size = 10} = { page: 0, size: 10 }
         } = {
-            filter: { genre : '', title : '', status : '' },
-            sort: {},
-            pagination: { page: 0, size: 7 }
+            filter: {  title: '', keywords: [], genre: '', genres: [], crew: [], cast: [], producers: [], releaseDate: '', status: '' },
+            sort: [],
+            pagination: { page: 0, size: 10 }
         }
     ) {
+        const data =  await fetch('/api/movies', {
+            method: 'GET',
+            headers: {
+                'Authorization': this.#token,
+                'Content-Type': 'application/json',
+                'Accepts': 'application/json'
+            }
+        })
+        return await data.json()
+
         return new Promise(resolve => {
             const filtered = DATA.movies
                 ?.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase() || ''))
@@ -58,18 +92,36 @@ export default class API {
 
             resolve(data)
         })
+
     }
+
+    //Get one movie
     async findMovie(id) {
-        return DATA.movies.find(movie => movie.id === id)
-    }
-    async findUser(id) {
-        return new Promise(resolve => {
-            const user = DATA.users.find(user => user.email === id)
-
-            resolve(user)
+        const data =  await fetch(`/api/movies/${id}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': this.#token,
+                'Content-Type': 'application/json',
+                'Accepts': 'application/json'
+            }
         })
+        return await data.json()
     }
 
+    //Get one user
+    async findUser(id = '') {
+        const data =  await fetch(`/api/users/${id}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': this.#token,
+                'Content-Type': 'application/json',
+                'Accepts': 'application/json'
+            }
+        })
+        return await data.json()
+    }
+
+    //Get assessments by movie or user
     async findComments(
         {
             filter: { movie = '', user = '' } = { movie: '', user: '' },
@@ -97,6 +149,7 @@ export default class API {
         })
     }
 
+    //Create an assessment
     async createComment(comment) {
         return new Promise(resolve => {
             DATA.comments.unshift(comment)
@@ -105,10 +158,12 @@ export default class API {
         })
     }
 
+    //Create an user
     async createUser(user) {
         console.log(user)
     }
 
+    //Update an user
     async updateUser(id, user) {
         console.log(user)
     }
